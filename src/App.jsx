880fd42ceebb9fcd2ea87d50ded5b884f3436ec2
adminLALIT <lalit.yathrthriti@gmail.com>
2023-08-27 22:50:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+import Intro from "./components/intro/Intro";
+import About from "./components/about/About";
+
+function App() {
+  return (
+    <div>
+      <Intro />
+      <About/>
+    </div>
+  );
+}
+
+export default App;
